295ac55caf3e917e57b1a40234f057c5692574df
cesarr340 <cesarr340@gmail.com>
2025-02-16 02:20:04 +0000

    começando a seção de boas vindas da aplicação

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { CurrencyDollar, CurrencyEur, CurrencyGbp, CurrencyJpy } from "@phosphor-icons/react";
 import Navbar from "../components/Navbar";
 import '../styles/home.scss'
 
@@ -5,6 +6,33 @@ export default function Home() {
     return (
         <>
             <Navbar />
+
+            <section id="hero">
+                <div className="currency-dots">
+                    <div>
+                        <CurrencyDollar size={32} weight="fill" color="#ffffffb6" />
+                    </div>
+
+                    <div>
+                        <CurrencyJpy size={32} weight="fill" color="#ffffffb6" />
+                    </div>
+                </div>
+
+                <div className="hero-title">
+                    <div><span>Remɐǝn</span> in one click</div>
+                    <div>Dive in Fast, Accurate, and Up-to-Date Exchange Rates</div>
+                </div>
+
+                <div className="currency-dots">
+                    <div>
+                        <CurrencyEur size={32} weight="fill" color="#ffffffb6" />
+                    </div>
+
+                    <div>
+                        <CurrencyGbp size={32} weight="fill" color="#ffffffb6" />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
