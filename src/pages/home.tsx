85624b60f216d2989085e65749853915d3940832
cesarr340 <cesarr340@gmail.com>
2025-02-17 15:14:29 +0000

    mudanças na seção de boas vindas e alterção da barra de rolagem da página

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { CurrencyDollar, CurrencyEur, CurrencyGbp, CurrencyJpy } from "@phosphor-icons/react";
+import { Coins } from "@phosphor-icons/react";
 import Navbar from "../components/Navbar";
 import '../styles/home.scss'
 
@@ -8,28 +8,17 @@ export default function Home() {
             <Navbar />
 
             <section id="hero">
-                <div className="currency-dots">
-                    <div>
-                        <CurrencyDollar size={32} weight="fill" color="#ffffffb6" />
-                    </div>
-
-                    <div>
-                        <CurrencyJpy size={32} weight="fill" color="#ffffffb6" />
-                    </div>
-                </div>
-
                 <div className="hero-title">
-                    <div><span>Remɐǝn</span> in one click</div>
-                    <div>Dive in Fast, Accurate, and Up-to-Date Exchange Rates</div>
-                </div>
-
-                <div className="currency-dots">
                     <div>
-                        <CurrencyEur size={32} weight="fill" color="#ffffffb6" />
+                        <span>Remɐǝn</span> in one click
                     </div>
 
                     <div>
-                        <CurrencyGbp size={32} weight="fill" color="#ffffffb6" />
+                        <Coins size={30} weight="duotone" />
+                        <div>
+                            Dive in Fast, Accurate, and Up-to-Date Exchange Rates
+                        </div>
+                        <Coins size={30} weight="duotone" />
                     </div>
                 </div>
             </section>
