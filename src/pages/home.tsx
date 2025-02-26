baf5de1bc972394495e1edeed1e53fb9e4fa4f05
cesarr340 <cesarr340@gmail.com>
2025-02-26 01:44:12 +0000

    seção about

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -22,6 +22,42 @@ export default function Home() {
                     </div>
                 </div>
             </section>
+
+            <section id="about">
+                <div className="about-subtitle">
+                    About
+                </div>
+
+                <div className="about-title">
+                    A tool designed to simplify conversion worldwide
+                </div>
+
+                <p>
+                    With Remɐǝn, converting currencies is as fast and intuitive as conducting an online search.
+                    Our platform provides accurate and real-time exchange rates,
+                    serving users across all continents for travel, business, or international transfers.
+                </p>
+
+                <div className="about-cards-container">
+                    <div className="card">
+                        <div className="card-title">Many Conversions</div>
+                        <div className="card-content">performed daily, ensuring accuracy and speed in exchange rates.</div>
+                    </div>
+                    <div className="card">
+                        <div className="card-title">99.99%</div>
+                        <div className="card-content">service availability so you can convert at any time.</div>
+                    </div>
+                    <div className="card">
+                        <div className="card-title">Support for over 50</div>
+                        <div className="card-content">currencies for instant and hassle-free conversions.</div>
+                    </div>
+                    <div className="card">
+                        <div className="card-title">Real-time Updates</div>
+                        <div className="card-content">so you always have access to the most accurate market rates.</div>
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }
